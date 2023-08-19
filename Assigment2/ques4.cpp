38c8f5f2c4d8624b6eb2f15dee9b8f26bf409984
ParthPidurkar <85994476+ParthPidurkar@users.noreply.github.com>
2023-08-19 11:24:08 +0530

    Add files via upload

---
+++ Assigment2/ques4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    int hight;
+    float v = 3.14*radius*radius*hight;
+    cout<<"\n volume of the cylinder is "<<v<<endl;
+    return 0;
+}
---
+++ Assigment2/ques5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a,b;
+    cout<<"\nEnter 1 char :";
+    cin>>a;
+    cout<<"\nEnter 2 char :";
+    cin>>b;
+    int x = (int)a;
+    int y = (int)b;
+    cout<<"Difference Between two char is "<<y-x<<endl;
+    return 0;
+}
